2344bf6d7ae5102ceb02abee61c2b49ca5cc1e35
German Dario <germangp088@gmail.com>
2022-08-23 10:04:53 -0300

    Added env vars & mint sales calculation.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import RichOutput from "rich-output";
+import "dotenv/config";
 import { getMagicEdenArithmeticAverage, getSolanArtArithmeticAverage } from "./marketplaces/index.js";
 import { calculateArithmeticAverage, calculatePercentage } from "./math.js";
 
@@ -8,5 +9,8 @@ const solanArtArithmeticAverage = await getSolanArtArithmeticAverage();
 const arithmeticAverage = calculateArithmeticAverage([magicEdenArithmeticAverage, solanArtArithmeticAverage]);
 console.log(`Media aritmetica de lanzamiento de colecciones total: ${RichOutput.green(arithmeticAverage)}.`);
 
-const estimatedQty = calculatePercentage(arithmeticAverage, 0.625)
-console.log(`\nTotal estimado de colecciones con plataforma de staking: ${RichOutput.green(estimatedQty)}.`);
+const estimatedStakingQty = calculatePercentage(arithmeticAverage, process.env.STAKING_PERCENTAJE)
+console.log(`\nTotal estimado de colecciones con plataforma de staking: ${RichOutput.green(estimatedStakingQty)}.`);
+
+const estimatedMintQty = calculatePercentage(arithmeticAverage, process.env.MINT_PERCENTAJE)
+console.log(`\nTotal estimado de colecciones con sitio de mint: ${RichOutput.green(estimatedMintQty)}.`);
