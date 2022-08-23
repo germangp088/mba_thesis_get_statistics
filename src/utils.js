452aafbb292f07581c99d91292dad3a868510844
German Dario <germangp088@gmail.com>
2022-08-22 21:29:30 -0300

    Build year basis dinamically.

--- src/utils.js
+++ src/utils.js
@@ -2,20 +2,25 @@ import fs from "fs";
 import { stringify } from "csv-stringify";
 
 const buildYearBasis = () => {
-    const yearBasisObject = [{ date: 'Agosto 2022' }, { date: 'Julio 2022' }, { date: 'Junio 2022' },
-        { date: 'Mayo 2022' }, { date: 'Abril 2022' }, { date: 'Marzo 2022' }, { date: 'Febrero 2022' },
-        { date: 'Enero 2022' }, { date: 'Diciembre 2021' }, { date: 'Noviembre 2021' },
-        { date: 'Octubre 2021' }, { date: 'Septiembre 2021' }
-    ]
+    const yearBasisObject = [];
 
-    const date = new Date('2022-09-01');
+    const monthNames = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+    const date = new Date();
+    date.setDate(1);
+
+    for (let index = 0; index <= 11; index++) {
+        yearBasisObject.push({ date: monthNames[date.getMonth()] + ' ' + date.getFullYear() })
+        date.setMonth(date.getMonth() - 1);
+    }
+
+    const now = new Date();
 
     for (let index = 0; index < yearBasisObject.length; index++) {
         const monthMetadata = yearBasisObject[index];
         
         const indexToGetPreviousMonth = -(index);
-        monthMetadata.startDate = new Date(Date.UTC(date.getFullYear(), date.getMonth() + indexToGetPreviousMonth, 1, 0, 0, 0)).toISOString();
-        monthMetadata.endDate = new Date(Date.UTC(date.getFullYear(), date.getMonth() + indexToGetPreviousMonth + 1, 0, 23, 59, 59)).toISOString();
+        monthMetadata.startDate = new Date(Date.UTC(now.getFullYear(), now.getMonth() + indexToGetPreviousMonth, 1, 0, 0, 0)).toISOString();
+        monthMetadata.endDate = new Date(Date.UTC(now.getFullYear(), now.getMonth() + indexToGetPreviousMonth + 1, 0, 23, 59, 59)).toISOString();
     }
 
     return yearBasisObject;
@@ -28,7 +33,7 @@ const exportToCsv = (columns, rows, filename = "results.xls") => {
     }
 
     const writableStream = fs.createWriteStream(`${folderName}/${filename}`);
-    const stringifier = stringify({ header: true, cast: { string: (str) => str.toLocaleUpperCase() }, columns: columns });
+    const stringifier = stringify({ header: true, columns: columns });
 
     rows.forEach(row => {
         stringifier.write(row);
